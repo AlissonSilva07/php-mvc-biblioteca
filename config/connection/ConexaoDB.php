ecf87fed154f10a1aa72b723b0a091db8d49d12e
Alisson Rego <alissondroid07@gmail.com>
2024-01-07 14:46:29 -0300

    Adicionada classe de conexão com o banco de dados

---
+++ config/connection/ConexaoDB.php
@@ -0,0 +1,26 @@
+<?php
+
+    namespace Alisson\config\connection;
+
+    use PDO;
+
+    class ConexaoDB {
+
+        private static $db_client = "mysql";
+		private static $db_host = "localhost";
+		private static $db_name = "livraria";
+		private static $db_user = "root";
+		private static $db_pass = "";
+
+        public static function ConexaoDB(): PDO {
+            try {
+                $pdo = new PDO(self::$db_client . ":host=" . self::$db_host . ";dbname=" . self::$db_name, self::$db_user, self::$db_pass);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                return $pdo;
+            } catch (PDOException e) {
+                echo 'ERROR: ' . $e->getMessage();
+            }
+        }
+
+    }
