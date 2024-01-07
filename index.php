f50e54a0ca4065dae7be57503b04930424f1b9ae
Alisson Rego <alissondroid07@gmail.com>
2024-01-07 16:13:35 -0300

    Remoção da index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    require_once 'autoload.php';
-
-    use Alisson\model\LivroModel;
-
-    $livro = new LivroModel(1, "NomeLivro", "AutorLivro", true, date("2014/05/02"), date("2014/05/10"));
-
-    var_dump($livro);
