e3ee128098b0bf8aaff5408513a8ba2c4d1067c6
Alisson Rego <alissondroid07@gmail.com>
2024-01-09 13:37:35 -0300

    Alterações no comando SQL de criação da tabela

--- config/sql/livros.sql
+++ config/sql/livros.sql
@@ -1,7 +1,7 @@
 CREATE TABLE livros (
-    idLivro INT NOT NULL,
+    idLivro INT NOT NULL AUTO_INCREMENT,
     nomeLivro VARCHAR(255) NOT NULL,
-    autorLivro VARCHAR(255),
+    autorLivro VARCHAR(255) NOT NULL,
     disponivel boolean NOT NULL,
     dataInicio DATE NOT NULL,
     dataDevolucao DATE NOT NULL,
